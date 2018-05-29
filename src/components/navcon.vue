a50c327cb8b8e686d5521f450eb9f76d7c26bbd2
lidonghui <donghui.li@sim.com>
2018-05-29 09:23:25 +0800

    修正头部导航显示问题

--- src/components/navcon.vue
+++ src/components/navcon.vue
@@ -7,7 +7,8 @@
       <img class="showimg" :src="collapsed?imgsq:imgshow" @click="toggle(collapsed)">
     </el-button>
     <el-submenu index="2" class="submenu">
-      <template slot="title">{{user.userRealName}}</template>
+      <!-- <template slot="title">{{user.userRealName}}</template> -->
+      <template slot="title">超级管理员</template>
       <el-menu-item index="2-1">设置</el-menu-item>
       <el-menu-item @click="content()" index="2-2">个人中心</el-menu-item>
       <el-menu-item @click="exit()" index="2-3">退出</el-menu-item>
